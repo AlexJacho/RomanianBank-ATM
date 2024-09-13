0d59d7fba5d244c1c8435b8f865a1c0aef678d7e
Alexander Jacho <alexjacho77@gmail.com>
2024-09-13 12:42:05 +0300

    Added functionality to greet users

--- src/main/java/com/RomanianBank/Main.java
+++ src/main/java/com/RomanianBank/Main.java
@@ -2,11 +2,6 @@ package com.RomanianBank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello RomanianBank");
-        try {
-            int result = 10 / 0; // Genera una excepción
-        } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir por cero!");
-        }
+        System.out.println("Welcome to Romanian Bank");
     }
 }
