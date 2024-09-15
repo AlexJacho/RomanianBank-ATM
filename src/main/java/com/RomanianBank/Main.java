4f26df3b092a0372b04396a2c44fc6ce757eb36d
Alexander Jacho <alexjacho77@gmail.com>
2024-09-15 07:58:12 +0300

    Performed a deposit operation to update the balance Verified access conditions

--- src/main/java/com/RomanianBank/Main.java
+++ src/main/java/com/RomanianBank/Main.java
@@ -2,10 +2,16 @@ package com.RomanianBank;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Bine ai venit la Romanian Bank");
+
         String numarCont = "14546549876";
         double soldFinal = 456.75;
         int pin = 1234;
         int[] transactii = {400, -300, 20};
-        System.out.println("Bine ai venit la Romanian Bank");
+
+        soldFinal += transactii[0]; //depozit
+        if (soldFinal > 0 && pin == 1234) {
+            System.out.println("Acces acordat");
+        }
     }
 }
